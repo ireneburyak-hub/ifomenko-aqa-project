07ae894d9481907d957cfa1ae559245f03cd4560
Iryna Fomenko <ireneburyak@gmail.com>
2026-09-20 16:26:20 +0200

    fixed locators in ProductDetails

--- page-objects/ProductDetails.page.js
+++ page-objects/ProductDetails.page.js
@@ -1,8 +1,8 @@
 export class ProductDetails {
     constructor(page) {
         this.page = page;
-        this.productName = page.locator('[data-test="inventory-item-name"]');
-        this.productPrice = page.locator('[data-test="inventory-item-price"]');
+        this.productName = page.locator('.inventory_details_name');
+        this.productPrice = page.locator('.inventory_details_price');
         this.addToCartButton= page.locator('[data-test="add-to-cart"]');
         this.backToProductButton= page.locator('[data-test="back-to-products"]');
     }
@@ -11,6 +11,9 @@ export class ProductDetails {
         await this.addToCartButton.click();
     }
     async getBikeLightInfo() {
+        console.log('Product names count:', await this.productName.count());
+        console.log('Product prices count:', await this.productPrice.count());
+
         return {
             name: await this.productName.innerText(),
             price: await this.productPrice.innerText()
